70f366565982f4ddb44e442b7788acfa3adb894a
LucasVicentee <lh201612@gmail.com>
2025-12-03 20:25:21 -0300

    inicializando o projeto

---
+++ src/main/java/com/LucasVicentee/SistemaEstoqueBackEnd/SistemaEstoqueBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.LucasVicentee.SistemaEstoqueBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEstoqueBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEstoqueBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LucasVicentee/SistemaEstoqueBackEnd/SistemaEstoqueBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LucasVicentee.SistemaEstoqueBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEstoqueBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
